5cb420f6e3f5192f43876c0e92858861c6163201
RiyadTangil <riyadkhanxp2@gmail.com>
2021-03-18 23:11:56 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>this is header page</h1>
+            <button className="btn btn-primary">hello</button>
+        </div>
+    );
+};
+
+export default Header;
